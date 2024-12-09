c25bc4f00bb14e4a1208ec70e074145f7f3e4114
cjbq <cjbquion@gmail.com>
2024-12-08 21:22:54 -0500

    init

---
+++ nfc_poll.c
@@ -0,0 +1,170 @@
+#include <stdlib.h>
+#include <nfc/nfc.h>
+#include <inttypes.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+#include <time.h>
+#include <errno.h>
+
+// gcc -o nfc_poll nfc_poll.c -lnfc
+
+static nfc_device *pnd = NULL;
+static nfc_context *context;
+
+static const uint8_t dragon_uid[10] = {0x04, 0x71, 0x8c, 0x94, 0x24, 0x02, 0x89};
+static const uint8_t tiger_uid[10] =   {0x04, 0xd1, 0x4a, 0x7e, 0x24, 0x02, 0x89};
+static const uint8_t stag_uid[10] =  {0x04, 0xe1, 0x43, 0x95, 0x24, 0x02, 0x89};
+
+/* msleep(): Sleep for the requested number of milliseconds. */
+int msleep(long msec)
+{
+    struct timespec ts;
+    int res;
+
+    if (msec < 0)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ts.tv_sec = msec / 1000;
+    ts.tv_nsec = (msec % 1000) * 1000000;
+
+    do
+    {
+        res = nanosleep(&ts, &ts);
+    } while (res && errno == EINTR);
+
+    return res;
+}
+
+static void
+print_hex(const uint8_t *pbtData, const size_t szBytes)
+{
+    size_t szPos;
+
+    for (szPos = 0; szPos < szBytes; szPos++)
+    {
+        printf("%02x  ", pbtData[szPos]);
+    }
+    printf("\n");
+}
+
+void poll(nfc_target *targets)
+{
+    int res;
+    const nfc_modulation nmMifare = {
+        .nmt = NMT_ISO14443A,
+        .nbr = NBR_106,
+    };
+    if ((res = nfc_initiator_list_passive_targets(pnd, nmMifare, targets, 16)) >= 0)
+    {
+        if (res > 0)
+        {
+            for (int n = 0; n < res; n++)
+            {
+                nfc_target target = targets[n];
+                if (target.nti.nai.szUidLen > 0)
+                {
+                    print_hex(target.nti.nai.abtUid, target.nti.nai.szUidLen);
+
+                    if (memcmp(&dragon_uid, &target.nti.nai.abtUid, target.nti.nai.szUidLen) == 0)
+                    {
+                        printf("DRAGON!\n");
+                        system("/Users/chrisquion/BookProjection/dragon.py");
+                    }
+                     if (memcmp(&stag_uid, &target.nti.nai.abtUid, target.nti.nai.szUidLen) == 0)
+                    {
+                        printf("STAG!\n");
+                        system("/Users/chrisquion/BookProjection/stag.py");
+                    }
+                    if (memcmp(&tiger_uid, &target.nti.nai.abtUid, target.nti.nai.szUidLen) == 0)
+                    {
+                        printf("TIGER!\n");
+                        system("/Users/chrisquion/BookProjection/tiger.py");
+                    }
+                }
+            }
+        }
+    }
+}
+
+static void stop_polling(int sig)
+{
+    printf("Exiting\n");
+    (void)sig;
+    if (pnd != NULL) {
+        nfc_abort_command(pnd);
+        nfc_exit(context);
+        exit(0);
+    }
+    else
+    {
+        nfc_exit(context);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    signal(SIGINT, stop_polling);
+    printf("Beginning poll\n");
+    int i = 0;
+    int time = 600;
+    while (i < time)
+    {
+        // Allocate only a pointer to nfc_context
+        nfc_context *context;
+
+        // Initialize libnfc and set the nfc_context
+        nfc_init(&context);
+        if (context == NULL)
+        {
+            printf("Unable to init libnfc (malloc)\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Display libnfc version
+        const char *acLibnfcVersion = nfc_version();
+        (void)argc;
+        //printf("%s uses libnfc %s\n", argv[0], acLibnfcVersion);
+
+        // Open, using the first available NFC device which can be in order of selection:
+        //   - default device specified using environment variable or
+        //   - first specified device in libnfc.conf (/etc/nfc) or
+        //   - first specified device in device-configuration directory (/etc/nfc/devices.d) or
+        //   - first auto-detected (if feature is not disabled in libnfc.conf) device
+        pnd = nfc_open(context, NULL);
+
+        if (pnd == NULL)
+        {
+            printf("ERROR: %s\n", "Unable to open NFC device.");
+            exit(EXIT_FAILURE);
+        }
+        // Set opened NFC device to initiator mode
+        if (nfc_initiator_init(pnd) < 0)
+        {
+            nfc_perror(pnd, "nfc_initiator_init");
+            exit(EXIT_FAILURE);
+        }
+
+        //printf("NFC reader: %s opened\n", nfc_device_get_name(pnd));
+        
+        nfc_target targets[16];
+        //printf("Beginning poll\n");
+        poll(targets);
+
+        // Close NFC device
+        nfc_close(pnd);
+        //printf("NFC device closed.");
+        // Release the context
+        nfc_exit(context);
+
+        i++;
+        msleep(200);
+    }
+
+    exit(EXIT_SUCCESS);
+}
---
+++ stag.py
@@ -0,0 +1,201 @@
+#! /usr/bin/env python3
+#
+# PyQt5 example for VLC Python bindings
+# Copyright (C) 2009-2010 the VideoLAN team
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+#
+"""
+A simple example for VLC python bindings using PyQt5.
+
+Author: Saveliy Yusufov, Columbia University, sy2685@columbia.edu
+Date: 25 December 2018
+"""
+
+import platform
+import os
+import sys
+
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QDesktopWidget
+
+import vlc
+
+class Player(QtWidgets.QMainWindow):
+    """A simple Media Player using VLC and Qt
+    """
+
+    def __init__(self, master=None):
+        QtWidgets.QMainWindow.__init__(self, master)
+        self.setWindowTitle("Media Player")
+
+        # Create a basic vlc instance
+        self.instance = vlc.Instance()
+
+        self.media = None
+
+        # Create an empty vlc media player
+        self.mediaplayer = self.instance.media_player_new()
+        self.event_manager = self.mediaplayer.event_manager()
+
+        self.create_ui()
+        self.is_paused = False
+
+    def create_ui(self):
+        """Set up the user interface, signals & slots
+        """
+        self.widget = QtWidgets.QWidget(self)
+        self.setCentralWidget(self.widget)
+        
+        # In this widget, the video will be drawn
+        if platform.system() == "Darwin": # for MacOS
+            self.videoframe = QtWidgets.QMacCocoaViewContainer(0)
+        else:
+            self.videoframe = QtWidgets.QFrame()
+            
+        self.palette = self.videoframe.palette()
+        self.palette.setColor(QtGui.QPalette.Window, QtGui.QColor(0, 0, 0))
+        self.videoframe.setPalette(self.palette)
+        self.videoframe.setAutoFillBackground(True)
+
+        self.vboxlayout = QtWidgets.QVBoxLayout()
+        self.vboxlayout.addWidget(self.videoframe)
+        self.widget.setLayout(self.vboxlayout)
+        menu_bar = self.menuBar()
+
+        # File menu
+        file_menu = menu_bar.addMenu("File")
+
+        # Add actions to file menu
+        open_action = QtWidgets.QAction("Load Video", self)
+        close_action = QtWidgets.QAction("Close App", self)
+        file_menu.addAction(open_action)
+        file_menu.addAction(close_action)
+
+        open_action.triggered.connect(self.open_file)
+        close_action.triggered.connect(sys.exit)
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.update_ui)
+        
+    def play_pause(self):
+        """Toggle play/pause status
+        """
+        if self.mediaplayer.play() == -1:
+            self.open_file()
+            return
+
+        self.mediaplayer.play()
+        self.timer.start()
+        
+    def move_screens(self):
+        monitor = QDesktopWidget().screenGeometry(1)
+        self.move(monitor.left(), monitor.top())
+
+    def resize_video(self):
+        monitor = QDesktopWidget().screenGeometry(1)
+        #self.videoframe.resize(1600, 900)
+        #self.videoframe.move(0, 0)
+        
+    def open_file(self):
+        """Open a media file in a MediaPlayer
+        """
+
+        dialog_txt = "Choose Media File"
+        filename = QtWidgets.QFileDialog.getOpenFileName(self, dialog_txt, os.path.expanduser('~'))
+        if not filename:
+            return
+
+        # getOpenFileName returns a tuple, so use only the actual file name
+        self.media = self.instance.media_new(filename[0])
+
+        # Put the media in the media player
+        self.mediaplayer.set_media(self.media)
+
+        # Parse the metadata of the file
+        self.media.parse()
+
+        # Set the title of the track as window title
+        self.setWindowTitle(self.media.get_meta(0))
+
+        # The media player has to be 'connected' to the QFrame (otherwise the
+        # video would be displayed in it's own window). This is platform
+        # specific, so we must give the ID of the QFrame (or similar object) to
+        # vlc. Different platforms have different functions for this
+        if platform.system() == "Linux": # for Linux using the X Server
+            self.mediaplayer.set_xwindow(int(self.videoframe.winId()))
+        elif platform.system() == "Windows": # for Windows
+            self.mediaplayer.set_hwnd(int(self.videoframe.winId()))
+        elif platform.system() == "Darwin": # for MacOS
+            self.mediaplayer.set_nsobject(int(self.videoframe.winId()))
+
+        self.play_pause()
+        
+    def total_quit(self, event):
+        self.close()
+        sys.exit(0) 
+        
+    def play_file(self, file_name):
+        """Open a media file in a MediaPlayer
+        """
+
+        # getOpenFileName returns a tuple, so use only the actual file name
+        self.media = self.instance.media_new(file_name)
+
+        # Put the media in the media player
+        self.mediaplayer.set_media(self.media)
+
+        # Parse the metadata of the file
+        self.media.parse()
+
+        # Set the title of the track as window title
+        self.setWindowTitle(self.media.get_meta(0))
+
+        # The media player has to be 'connected' to the QFrame (otherwise the
+        # video would be displayed in it's own window). This is platform
+        # specific, so we must give the ID of the QFrame (or similar object) to
+        # vlc. Different platforms have different functions for this
+        if platform.system() == "Linux": # for Linux using the X Server
+            self.mediaplayer.set_xwindow(int(self.videoframe.winId()))
+        elif platform.system() == "Windows": # for Windows
+            self.mediaplayer.set_hwnd(int(self.videoframe.winId()))
+        elif platform.system() == "Darwin": # for MacOS
+            self.mediaplayer.set_nsobject(int(self.videoframe.winId()))
+
+        self.event_manager.event_attach(vlc.EventType.MediaPlayerEndReached, self.total_quit)
+
+        self.play_pause()
+        
+    def update_ui(self):
+        """Updates the user interface"""
+        # No need to call this function if nothing is played
+        if not self.mediaplayer.is_playing():
+            self.timer.stop()
+
+def main():
+    """Entry point for our simple vlc player
+    """
+    app = QtWidgets.QApplication(sys.argv)
+    player = Player()
+    player.move_screens()
+    player.resize_video()
+
+    player.showFullScreen()
+    player.play_file("Stag.mp4")
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
